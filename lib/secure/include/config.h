495b92b0356242e3e78264b83426b810fdff4da2
Dee J. Mann <deemann@wolvesden.dynu.net>
2018-12-13 19:26:38 -0500

    Added Crafting

--- lib/secure/include/config.h
+++ lib/secure/include/config.h
@@ -12,17 +12,17 @@
 #define DISABLE_INTERMUD         0
 #define DISABLE_IMC2             0
 #define MINUTES_REBOOT_WARNING   20
-#define TIME_TO_RESET            300
+#define TIME_TO_RESET            1800
 #define RESET_ALL                0
 #define GMT_OFFSET               -32000
 #define LOCAL_TIME               0
-#define EXTRA_TIME_OFFSET        0
+#define EXTRA_TIME_OFFSET        5
 #define MAX_LOG_SIZE             150000
 #define MAX_NET_DEAD_TIME        1800
 #define MIN_HISTORY_SIZE         50
 #define MAX_HISTORY_SIZE         100
 #define LOGON_TIMEOUT            360
-#define IDLE_TIMEOUT             600
+#define IDLE_TIMEOUT             1800
 #define LINK_WAIT_PERIOD         1800
 #define MAX_PASSWORD_TRIES       3
 #define MIN_USER_NAME_LENGTH     2
@@ -53,7 +53,7 @@
 #define PING_INTERVAL            900
 #define ENABLE_ENCUMBRANCE       1
 #define GLOBAL_MONITOR           0
-#define PLAYER_KILL              0
+#define PLAYER_KILL              1
 #define NPC_CATCH_TELL_DEBUG     0
 #define SAME_IP_MAX              4
 #define COMPAT_MODE              1
@@ -83,9 +83,9 @@
 #define SEFUN_PLURALIZE          0
 #define LOG_REMOTE_CHANS         0
 #define LOG_LOCAL_CHANS          1
-#define ENABLE_CGI               0
-#define WWW_DIR_LIST             0
-#define ENABLE_CREWEB            0
+#define ENABLE_CGI               1
+#define WWW_DIR_LIST             1
+#define ENABLE_CREWEB            1
 #define F_TERMINAL_COLOR         1
 #define MAX_CALL_OUTS            750
 #define REQUIRE_QUESTING         1
@@ -101,6 +101,6 @@
 #define GRID                     1
 #define ENABLE_INSTANCES         0
 #define CED_DISABLED             1
-#define GUEST_ALLOWED            1
-#define PLAYER_INTERTELL_ALLOWED 0
+#define GUEST_ALLOWED            0
+#define PLAYER_INTERTELL_ALLOWED 1
 #endif /* s_config_h */
